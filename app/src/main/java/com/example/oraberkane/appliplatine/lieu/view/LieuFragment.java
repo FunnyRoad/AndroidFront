6bdf66415600209caa62b8646856e6494d9143d3
AMEUR <samy.ameur@capgemini.com>
2017-02-05 19:01:26 +0100

    Commit exceptionnel sur dev mais avancement au niveau de l'écran d'urgence

---
+++ app/src/main/java/com/example/oraberkane/appliplatine/lieu/view/LieuFragment.java
@@ -0,0 +1,30 @@
+package com.example.oraberkane.appliplatine.lieu.view;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.oraberkane.appliplatine.R;
+
+/**
+ * Created by sameur on 05/02/2017.
+ */
+
+public class LieuFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Retourne votre fichier layout
+        // Changer R.layout.yourlayoutfilename pour vos fragments
+        return inflater.inflate(R.layout.lieux_fragment, container, false);
+    }
+
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // Vous pouvez changer le titre dans la toolbar de vos differents fragments
+        getActivity().setTitle("Tour Eiffel");
+    }
+}
