87f28efc222b3d65793036596035de485fa53a8f
Aberkane Morjane <oum-rjal.aberkane@altran.com>
2017-02-11 21:17:55 +0100

    branch dev corrigé

--- app/src/main/java/com/example/oraberkane/appliplatine/roadtrip/view/RoadTripFragment.java
+++ app/src/main/java/com/example/oraberkane/appliplatine/roadtrip/view/RoadTripFragment.java
@@ -4,22 +4,37 @@ package com.example.oraberkane.appliplatine.roadtrip.view;
  * Created by sameur on 02/02/2017.
  */
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.oraberkane.appliplatine.MainActivity;
 import com.example.oraberkane.appliplatine.R;
+import com.example.oraberkane.appliplatine.googlemap.view.ActivityNewRoaTripRouteChoice;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class RoadTripFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Retourne votre fichier layout
         // Changer R.layout.yourlayoutfilename pour vos fragments
-        return inflater.inflate(R.layout.road_trip_fragment, container, false);
+        View v = inflater.inflate(R.layout.road_trip_fragment, container, false);
+        ButterKnife.bind(this,v);
+        return v;
     }
 
+
+    @OnClick(R.id.fab)
+    public void goToNewRoad(){
+        Intent intent = new Intent(getActivity(), ActivityNewRoaTripRouteChoice.class);
+        startActivity(intent);
+    }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
